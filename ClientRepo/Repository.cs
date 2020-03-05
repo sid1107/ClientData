d6c4fda2650bc3a78361f95510a1c92e0029267c
s.d.shrivastava <s.d.shrivastava@DDC2-L-8381KW2>
2020-03-05 11:03:37 +0530

    Added Client Repo

---
+++ ClientRepo/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientRepo
+{
+    public class Repository
+    {
+
+        public int Clientid { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string location { get; set; }
+        public string totemp { get; set; }
+    }
+}
