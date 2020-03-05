986386ee65e76c169126df45cb14159ab4927d30
s.d.shrivastava <s.d.shrivastava@DDC2-L-8381KW2>
2020-03-05 11:50:23 +0530

    Presented the Client Record with Dummy Data

---
+++ ClientData.Data/IClientRecord.cs
@@ -0,0 +1,34 @@
+﻿using ClientRepo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClientData.Data
+{
+    public interface IClientRecord
+    {
+        IEnumerable<Repository> getALL();
+    }
+
+    public class InMemoryCientData : IClientRecord
+    {
+        readonly List<Repository> repo;
+        public InMemoryCientData()
+        {
+            repo = new List<Repository>()
+            {
+                new Repository { Clientid =1,ClientName="Google",location="India",totemp="50"},
+                new Repository { Clientid =2,ClientName="Adidas",location="USA",totemp="250"},
+                new Repository { Clientid =3,ClientName="Reebok",location="Germany",totemp="1050"},
+                new Repository { Clientid =4,ClientName="Merc",location="Japan",totemp="9000"}
+            };
+        }
+        public IEnumerable<Repository> getALL()
+        {
+                return from rec in repo
+                   orderby rec.Clientid
+                    select rec;
+        }
+    }
+}
--- ClientData/Pages/ClientRecords/List.cshtml.cs
+++ ClientData/Pages/ClientRecords/List.cshtml.cs
@@ -2,15 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClientData.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using ClientRepo;
 
 namespace ClientData.Pages.ClientRecords
 {
     public class ListModel : PageModel
     {
+        private readonly IClientRecord clientRec;
+        public IEnumerable<Repository> ClientRepo { get; set; }
+
+        public ListModel(IClientRecord ClientRec)
+        {
+            this.clientRec = ClientRec;
+        }
         public void OnGet()
         {
+            ClientRepo = clientRec.getALL();
         }
     }
 }
