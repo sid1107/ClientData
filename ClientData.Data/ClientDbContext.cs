a7b7a1d35ffa7adb40ed1a86e912d340581b5841
s.d.shrivastava <s.d.shrivastava@DDC2-L-8381KW2>
2020-03-05 16:43:17 +0530

    Adding DbContext

---
+++ ClientData.Data/ClientDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ClientRepo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientData.Data
+{
+    public class ClientDbContext : DbContext
+    {
+        public DbSet<Repository> ClientData { get; set; }
+
+        public ClientDbContext(DbContextOptions<ClientDbContext> options)
+           : base(options)
+        {
+
+        }
+    }
+}
